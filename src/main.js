755ccbebf70476cdccadf1962ed8d3766e212fb2
Thomas <rejectmusician@hotmail.co.uk>
2017-03-14 01:44:34 +0000

    Changed some of the placeholder items to make use of new postCSS steps.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import App from './modules/App.jsx';
 
-require('../scss/base.scss');
+require('./base.scss');
 
 ReactDOM.render(<App />, document.getElementById('app'));

461916b87d1db2b68c5e5fcf71e023856a09e735
Thomas <rejectmusician@hotmail.co.uk>
2017-03-14 10:08:38 +0000

    Removed commented out import and console.logging statement.

--- src/modules/App.jsx
+++ src/modules/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-// import styles from './zapp.css';
+
 const styles = require('./zapp.scss');
 
 export default class App extends React.Component {
   render() {
-    console.log(styles);
     return (
       <div className={styles.root}>
         <h1>insert title here</h1>

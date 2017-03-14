8590848d218f5b1b370956c6585f6c66d28a90bb
Thomas <rejectmusician@hotmail.co.uk>
2017-03-14 01:43:38 +0000

    Shuffled file hierarchy to make it separate by module rather than by js/css.

--- src/modules/App.jsx
+++ src/modules/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+// import styles from './zapp.css';
+const styles = require('./zapp.scss');
 
 export default class App extends React.Component {
   render() {
+    console.log(styles);
     return (
-      <div className="my-class">
+      <div className={styles.root}>
         <h1>insert title here</h1>
       </div>
     );

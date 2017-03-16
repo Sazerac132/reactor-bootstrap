a3a3e9bcd2c796506ffa1b922d3f98cde19ff503
Thomas <rejectmusician@hotmail.co.uk>
2017-03-16 00:02:56 +0000

    Added require of index.html to main.js so webpack will bundle index.html too.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './modules/App/App.jsx';
 
+require('./index.html');
 require('./base.scss');
 
 ReactDOM.render(<App />, document.getElementById('app'));
